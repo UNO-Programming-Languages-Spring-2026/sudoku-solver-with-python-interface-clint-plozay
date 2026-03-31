7824d04fe00dfa8779f3c141e7dbce1d9b531555
Clint Plozay <ctwilb88@gmail.com>
2026-03-30 21:47:12 -0500

    questions 1-5

--- sudoku1.py
+++ sudoku1.py
@@ -1,10 +1,19 @@
 import sys, clingo
+from sudoku_board import Sudoku
 class ClingoApp(clingo.application.Application):
     def main(self, ctl, files):
+        ctl.load(r'..\sudoku\sudoku2.lp')
         for f in files:
             ctl.load(f)
         if not files:
             ctl.load("-")
         ctl.ground()
         ctl.solve()
+
+    def print_model(self, model, printer):
+        symbols = sorted(model.symbols(shown=True))
+        print(" ".join(str(s) for s in symbols))
+        sys.stdout.flush()
+
+    
 clingo.application.clingo_main(ClingoApp())
